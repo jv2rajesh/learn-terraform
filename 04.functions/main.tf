9b14cf8811b50c2103d9b8e8068f8f37fdbbb49d
Rajesh Reddy <jv2rajesh@gmail.com>
2023-08-08 19:51:02 +0530

    function

--- 04.functions/main.tf
+++ 04.functions/main.tf
@@ -18,5 +18,5 @@ output "fruit_stock" {
 }
 
 output "fruit_stock_price" {
-  value = lookup(var.fruit_with_stock["appple"], "price", 2)
+  value = lookup(var.fruit_with_stock["apple"], "price", 2)
 }
