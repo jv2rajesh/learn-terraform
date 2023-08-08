18258b74e44f46c443123266d6a6e117c3693871
Rajesh Reddy <jv2rajesh@gmail.com>
2023-08-08 15:02:56 +0530

    added security group

--- 02.ec2/server&dns record.tf
+++ 02.ec2/server&dns record.tf
@@ -1,18 +1,18 @@
-resource "aws_instance" "test" {
+resource "aws_instance" "rajesh" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
   vpc_security_group_ids = [ "sg-0e65f0ea76c159049" ]
 
   tags = {
-    Name = "test"
+    Name = "rajesh"
   }
 }
-resource "aws_route53_record" "test" {
+resource "aws_route53_record" "rajesh" {
   zone_id = "Z007817518CXIKGSGIF7E"
-  name    = "test-dev.jv2rajesh.online"
+  name    = "rajesh-dev.jv2rajesh.online"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.test.private_ip]
+  records = [aws_instance.rajesh.private_ip]
 }
 
 #  vpc_security_group_ids = [ "sg-0e65f0ea76c159049" ]  is a Security "group id" which we discussed
