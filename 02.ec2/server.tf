7cc972a672f69153c8e9ed891dc04934928b64be
Rajesh Reddy <jv2rajesh@gmail.com>
2023-08-08 14:42:06 +0530

    added security group

--- 02.ec2/server.tf
+++ 02.ec2/server.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "test" {
+resource "aws_instance" "Helloworld" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
   vpc_security_group_ids = [ "sg-0e65f0ea76c159049" ]
@@ -7,9 +7,9 @@ resource "aws_instance" "test" {
     Name = "HelloWorld"
   }
 }
-resource "aws_route53_record" "test" {
+resource "aws_route53_record" "Helloworld" {
   zone_id = "Z007817518CXIKGSGIF7E"
-  name    = "HelloWorld-dev.jv2rajesh.online"
+  name    = "HelloWorld.jv2rajesh.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.HelloWorld.private_ip]
