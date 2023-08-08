cdd29619071848f7e259f81fbacaf74e29c2b40f
Rajesh Reddy <jv2rajesh@gmail.com>
2023-08-08 19:44:11 +0530

    function

---
+++ 04.functions/main.tf
@@ -0,0 +1,18 @@
+variable "fruits" {
+  default = [ "apple", "banana"]
+}
+output "fruit" {
+  value = element(var.fruits,2)
+}
+
+variable "fruit_with_stock" {
+  default = {
+    apple = {
+      stock = 100
+    }
+  }
+}
+
+output "fruit_stock" {
+  value = try(var.fruit_with_stock.banana,0)
+}
