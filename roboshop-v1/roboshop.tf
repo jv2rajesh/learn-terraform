153f30755ae573165c9bf7ab5a976e35b84cff44
Rajesh Reddy <jv2rajesh@gmail.com>
2023-08-08 11:59:50 +0530

    security group added

--- roboshop-v1/roboshop.tf
+++ roboshop-v1/roboshop.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0e65f0ea76c159049" ]
 
   tags = {
     Name = "frontend"
@@ -10,6 +11,7 @@ resource "aws_instance" "frontend" {
 resource "aws_instance" "mongodb" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0e65f0ea76c159049" ]
 
   tags = {
     Name = "mongodb"
@@ -18,6 +20,7 @@ resource "aws_instance" "mongodb" {
 resource "aws_instance" "catalogue" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0e65f0ea76c159049" ]
 
   tags = {
     Name = "catalogue"
@@ -26,6 +29,7 @@ resource "aws_instance" "catalogue" {
 resource "aws_instance" "redis" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0e65f0ea76c159049" ]
 
   tags = {
     Name = "redis"
@@ -34,6 +38,7 @@ resource "aws_instance" "redis" {
 resource "aws_instance" "user" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0e65f0ea76c159049" ]
 
   tags = {
     Name = "user"
@@ -42,6 +47,7 @@ resource "aws_instance" "user" {
 resource "aws_instance" "mysql" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0e65f0ea76c159049" ]
 
   tags = {
     Name = "mysql"
@@ -50,6 +56,7 @@ resource "aws_instance" "mysql" {
 resource "aws_instance" "shipping" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0e65f0ea76c159049" ]
 
   tags = {
     Name = "shipping"
@@ -58,6 +65,7 @@ resource "aws_instance" "shipping" {
 resource "aws_instance" "rabbitmq" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0e65f0ea76c159049" ]
 
   tags = {
     Name = "rabbitmq"
@@ -66,6 +74,7 @@ resource "aws_instance" "rabbitmq" {
 resource "aws_instance" "payment" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0e65f0ea76c159049" ]
 
   tags = {
     Name = "payment"
@@ -74,6 +83,7 @@ resource "aws_instance" "payment" {
 resource "aws_instance" "cart" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0e65f0ea76c159049" ]
 
   tags = {
     Name = "cart"
