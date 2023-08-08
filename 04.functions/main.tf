c913dd07f9908ef7136aec848b9cecbd1ab6bf1e
Rajesh Reddy <jv2rajesh@gmail.com>
2023-08-08 19:48:58 +0530

    function

--- 04.functions/main.tf
+++ 04.functions/main.tf
@@ -2,7 +2,7 @@ variable "fruits" {
   default = [ "apple", "banana"]
 }
 output "fruit" {
-  value = element(var.fruits,1)
+  value = element(var.fruits,2)
 }
 
 variable "fruit_with_stock" {
@@ -15,4 +15,8 @@ variable "fruit_with_stock" {
 
 output "fruit_stock" {
   value = try(var.fruit_with_stock.banana,0)
+}
+
+output "fruit_stock_price" {
+  value = lookup(var.fruit_with_stock.apple.price,2)
 }
