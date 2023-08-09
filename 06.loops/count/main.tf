beafacf63192324a2d7871480ba37b04575c7d71
Rajesh Reddy <jv2rajesh@gmail.com>
2023-08-09 11:01:42 +0530

    loops

--- 06.loops/count/main.tf
+++ 06.loops/count/main.tf
@@ -1,8 +1,10 @@
 variable "components" {
   default = ["frontend", "mongodb"]
 }
-count = length(var.components)
 resource "aws_instance" "instance" {
+
+  count = length(var.components)
+
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
   vpc_security_group_ids = [ "sg-0e65f0ea76c159049" ]
