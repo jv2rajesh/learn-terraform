ea45479f0a0344035b1104c5ebd84ec62998c422
Rajesh Reddy <jv2rajesh@gmail.com>
2023-08-09 12:14:24 +0530

    for-each

--- 06.loops/for-each/main.tf
+++ 06.loops/for-each/main.tf
@@ -6,8 +6,10 @@ variable "components" {
 }
 
 resource "aws_security_group" "allow_tls" {
+  for_each = var.components
   name = lookup(var.components, each.value["name"], null)
 }
+
 
 
 # keys are frontend, mongodb etc
