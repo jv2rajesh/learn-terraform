1baf06f51b6254589956b6c827eade1c91e2ea48
Rajesh Reddy <jv2rajesh@gmail.com>
2023-08-09 13:55:22 +0530

    for-each

--- 06.loops/for-each/main.tf
+++ 06.loops/for-each/main.tf
@@ -7,7 +7,7 @@ variable "components" {
 
 resource "aws_security_group" "allow_tls" {
   for_each = var.components
-  name = lookup(var.components, each.value["name"], null)
+  name = lookup(each.value["name"], null)
 }
 
 
---
+++ roboshop-v2/roboshop.tf
@@ -0,0 +1,23 @@
+
+variable "ami" {
+
+}
+
+
+
+resource "aws_instance" "frontend" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0e65f0ea76c159049" ]
+
+  tags = {
+    Name = "frontend"
+  }
+}
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z007817518CXIKGSGIF7E"
+  name    = "frontend-dev.jv2rajesh.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
