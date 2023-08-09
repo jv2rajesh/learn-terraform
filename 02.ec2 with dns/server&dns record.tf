687796b25775289cc8d1b16aa09d0fe237dc0927
Rajesh Reddy <jv2rajesh@gmail.com>
2023-08-09 15:31:49 +0530

    for-each

--- 02.ec2 with dns/server&dns record.tf
+++ 02.ec2 with dns/server&dns record.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "rajesh" {
+resource "aws_instance" "instance" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
   vpc_security_group_ids = [ "sg-0e65f0ea76c159049" ]
@@ -7,12 +7,12 @@ resource "aws_instance" "rajesh" {
     Name = "rajesh"
   }
 }
-resource "aws_route53_record" "rajesh" {
+resource "aws_route53_record" "record" {
   zone_id = "Z007817518CXIKGSGIF7E"
   name    = "rajesh-dev.jv2rajesh.online"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.rajesh.private_ip]
+  records = [aws_instance.instance.private_ip]
 }
 
 #  vpc_security_group_ids = [ "sg-0e65f0ea76c159049" ]  is a Security "group id" which we discussed
