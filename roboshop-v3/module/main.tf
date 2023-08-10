1ada9d898d62f8225871a6d06181e142b73f51da
Rajesh Reddy <jv2rajesh@gmail.com>
2023-08-10 13:08:49 +0530

    creating instances and records in V1

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -20,6 +20,6 @@ resource "aws_route53_record" "record" {
 
 data "aws_ami" "ami" {
   most_recent      = true
-  name_regex       = "Centos-*"
+  name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
 }
