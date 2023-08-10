8e88ae606dc2d8df49387dfbf65fcf4bb3b6207f
Rajesh Reddy <jv2rajesh@gmail.com>
2023-08-10 16:29:24 +0530

    provisioner and module in terraform

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -30,7 +30,6 @@ cd /home/centos/roboshop-ansible
 git pull
 sleep 30
 ansible-playbook -i ${var.name}.jv2rajesh.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
-
 EOF
   }
 }
