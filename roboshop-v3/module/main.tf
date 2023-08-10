45591f1133b777d5f101a82d1f514a664de7b08b
Rajesh Reddy <jv2rajesh@gmail.com>
2023-08-10 18:00:44 +0530

    provisioner and module in terraform

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -28,7 +28,7 @@ resource "null_resource" "ansible" {
     command = <<EOF
 cd /home/centos/roboshop-ansible
 git pull
-sleep 30
+sleep 60
 ansible-playbook -i ${var.name}-dev.jv2rajesh.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
 EOF
   }
