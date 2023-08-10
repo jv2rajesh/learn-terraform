da1f59320fab1266f4d9a610cab75e8ebc6d2c74
Rajesh Reddy <jv2rajesh@gmail.com>
2023-08-10 13:05:09 +0530

    creating instances and records in V1

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
 
-  ami           = data.aws_ami_id
-  instance_type = var.instance_type
+  ami                    = data.aws_ami_id
+  instance_type          = var.instance_type
   vpc_security_group_ids = var.security_groups
 
   tags = {
