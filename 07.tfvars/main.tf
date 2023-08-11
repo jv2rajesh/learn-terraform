ea1dd9ffaf6af1a85e880af3c3f14f78e26c41ba
Rajesh Reddy <jv2rajesh@gmail.com>
2023-08-11 12:28:04 +0530

    provisioner and module in terraform

--- 07.tfvars/main.tf
+++ 07.tfvars/main.tf
@@ -3,3 +3,8 @@ variable "test" {}
 output "test" {
   value = var.test
 }
+
+
+
+/*  command: terraform apply -auto-approve -var-file=dev.tfvars*/
+/*  command: terraform apply -auto-approve -var-file=prod.tfvars*/
